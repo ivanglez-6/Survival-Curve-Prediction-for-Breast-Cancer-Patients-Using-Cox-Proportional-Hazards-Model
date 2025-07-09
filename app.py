57823294941594088e5d854f02b5ccc541959631
ivanglez-6 <ivanguribe4862@gmail.com>
2025-07-09 15:33:41 -0600

    First home design

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
+
+os.chdir("C:/Users/Usuario/OneDrive/surv_flask")
 
 @app.route("/")
 def home():
-    return "Hola"
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
